dfd466d5fa9d99cdc83d9927b051e7af64aa2193
Rodrigo Kinob Torres Campos <rkinob@gmail.com>
2022-06-21 15:05:02 -0300

    fix: alteração server.ts

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lojagi-app'));
+/*app.use(express.static('./dist/lojagi-app'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/lojagi-app/'}),
-);
+);*/
+
+app.use(express.static(path.join(__dirname, 'dist','lojagi-app')));
+
+res.sendFile(path.join(__dirname,'dist','lojagi-app','index.html'));
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
