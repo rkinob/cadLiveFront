f855617f62ea566885a133b620e75f9619aa4bda
Rodrigo Kinob Torres Campos <rkinob@gmail.com>
2022-06-21 16:05:31 -0300

    fix: alteração server.ts

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist','lojagi-app')));
+app.use(express.static(path.join(__dirname, 'dist','lojaGiFront')));
 
 app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname,'dist','lojagi-app','index.html'))
+  res.sendFile(path.join(__dirname,'dist','lojaGiFront','index.html'))
 );
 
 // Start the app by listening on the default Heroku port
